99a2b89a60ae5bea43fe2405ecb8fea9986583b1
PPquoc <quocpp1996@gmail.com>
2018-11-07 14:12:47 +0700

    7/11/2018

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,20 +2,27 @@ import React,{Component} from 'react';
 import {View,Text,ScrollView} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumsDetail';
+import Spinner from './Spinner';
 
 //create component
 class AlbumList extends Component {
 
     //
-    state = { albums: [],  };
-
+    state = { albums: [],  loading:false};
 
     componentWillMount(){
+        this.setState({loading:true});
         console.log('componentWillMount in albumlist');
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => this.setState({albums: response.data}));
+            .then(response => this.setState({albums: response.data,loading:false}));
     };
+
+
     renderAlbums(){
+        if(this.state.loading){
+                return <Spinner size={'large'}/>;
+        }
+
        return this.state.albums.map(album_atList_renderAlbum => <AlbumDetail key={album_atList_renderAlbum.title}  album_from_albumlist= {album_atList_renderAlbum}/>);
     }
    render(){
